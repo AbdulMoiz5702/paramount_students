d93fd4844ce50bc28f1f49636edb1c9367bedc65
AbdulMoiz5702 <abdulmoizkoikenx10@gmail.com>
2025-05-19 14:08:19 +0500

    Apis_added

--- lib/core/services/Network_services/app_apis.dart
+++ lib/core/services/Network_services/app_apis.dart
@@ -2,6 +2,7 @@
 
 class AppApis {
 
+  // Auth Apis
   static const String  _ip = '192.168.1.28';
   static const String _baseUrl = 'http://$_ip:8001/api';
   static const String signup = '$_baseUrl/auth/signup';
@@ -9,5 +10,9 @@ class AppApis {
   static const String confirmOtp = '$_baseUrl/auth/confirm_otp?';
   static const String resendOtp = '$_baseUrl/auth/resend_otp?username=';
   static const String forgotPassword = '$_baseUrl/auth/forgot_password?username=';
+
+// Users Apis
+  static const String getUsers = '$_baseUrl/v1/users';
+  static const String getSingleUser = '$_baseUrl/v1/users/';
 
 }
