0af1c057f8616e463a96a5d33afc3852e56873ee
AbdulMoiz5702 <abdulmoizkoikenx10@gmail.com>
2025-05-26 13:42:46 +0500

    user profile screen

--- lib/views/Porfile/Profile_Screen.dart
+++ lib/views/Porfile/Profile_Screen.dart
@@ -1,46 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:paramount_student/core/helper_fuctions/current_access_token.dart';
+import 'package:paramount_student/views/common/custom_button.dart';
 import 'package:paramount_student/views/common/error_widgte.dart';
 import 'package:paramount_student/views/common/shimmer_widget.dart';
 import '../../bloc/Profile/profile_bloc.dart';
 import '../../bloc/Profile/profile_event.dart';
 import '../../bloc/Profile/profile_state.dart';
 
-
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
   @override
   Widget build(BuildContext context) {
-   Future.microtask((){
-     context.read<ProfileBloc>().add(GetCurrentUser(id: int.parse(CurrentUserSecrets.currentUserId)));
-   });
+    Future.microtask(() {
+      context.read<ProfileBloc>().add(
+        GetCurrentUser(id: int.parse(CurrentUserSecrets.currentUserId)),
+      );
+    });
     return Scaffold(
       appBar: AppBar(title: Text("User Profile")),
       body: BlocBuilder<ProfileBloc, ProfileState>(
         builder: (context, state) {
           if (state.onGetUser == true) {
-            return ShimmerScreen(isProfileScreen: true,);
-          }else if (state.user == null){
-            return ErrorRetryWidget(errorMessage: state.errorMessage, onRetry: (){});
-          }else {
+            return ShimmerScreen(isProfileScreen: true);
+          } else if (state.user == null) {
+            return ErrorRetryWidget(
+              errorMessage: state.errorMessage,
+              onRetry: () {},
+            );
+          } else {
             final user = state.user!;
-            return  Padding(
+            return Padding(
               padding: EdgeInsets.all(16),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   BlocBuilder<ProfileBloc, ProfileState>(
-                    buildWhen: (pre,current)=> pre.image != current.image,
-                      builder: (context,state){
-                        if (state.image != null){
-                          return CircleAvatar(radius: 40,backgroundImage:FileImage(state.image!,),);
-                        }else{
-                          return CircleAvatar(radius: 40,backgroundImage:NetworkImage(user.profilePicture!,),);
-                        }
-                      }),
+                    buildWhen: (pre, current) => pre.image != current.image,
+                    builder: (context, state) {
+                      if (state.image != null) {
+                        return GestureDetector(
+                          onTap: () {
+                            context.read<ProfileBloc>().add(
+                              PickUserProfilePic(context: context),
+                            );
+                          },
+                          child: CircleAvatar(
+                            radius: 40,
+                            backgroundImage: FileImage(state.image!),
+                          ),
+                        );
+                      } else if (user.profilePicture != null) {
+                        return GestureDetector(
+                          onTap: () {
+                            context.read<ProfileBloc>().add(
+                              PickUserProfilePic(context: context),
+                            );
+                          },
+                          child: CircleAvatar(
+                            radius: 40,
+                            backgroundImage: NetworkImage(user.profilePicture!),
+                          ),
+                        );
+                      } else {
+                        return CircleAvatar(
+                          radius: 40,
+                          child: TapIcon(
+                            iconData: Icons.person,
+                            size: 35,
+                            onTap: () {
+                              context.read<ProfileBloc>().add(
+                                PickUserProfilePic(context: context),
+                              );
+                            },
+                          ),
+                        );
+                      }
+                    },
+                  ),
                   SizedBox(height: 8),
-                  Text('Name: ${user.firstName} ${user.lastName}', style: TextStyle(fontSize: 20)),
+                  Text(
+                    'Name: ${user.firstName} ${user.lastName}',
+                    style: TextStyle(fontSize: 20),
+                  ),
                   SizedBox(height: 8),
                   Text('Email: ${user.email}'),
                   SizedBox(height: 8),
@@ -50,7 +92,6 @@ class ProfileScreen extends StatelessWidget {
               ),
             );
           }
-
         },
       ),
     );
