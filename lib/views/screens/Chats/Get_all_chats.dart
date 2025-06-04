4781c95f2a8d70b749588cb9c5024557f7483b3d
AbdulMoiz5702 <abdulmoizkoikenx10@gmail.com>
2025-06-04 14:25:27 +0500

    Chat_Bloc_and_screen

--- lib/views/screens/Chats/Get_all_chats.dart
+++ lib/views/screens/Chats/Get_all_chats.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:paramount_student/bloc/Chats/chat_bloc.dart';
+import 'package:paramount_student/bloc/Chats/chat_event.dart';
+import 'package:paramount_student/core/helper_fuctions/date_formatter.dart';
+import 'package:paramount_student/core/helper_fuctions/helper_function.dart';
+
+import '../../../core/presentation/app_colors.dart';
+import '../../common/custom_sizedBox.dart';
+import '../../common/shimmer_widget.dart';
+import '../../common/text_widgets.dart';
 
 
 class GetAllChatsScreen extends StatelessWidget {
@@ -6,6 +16,90 @@ class GetAllChatsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Future<void> loadAllData(BuildContext context) async {
+      context.read<ChatBloc>().add(GetAllChats());
+      await Future.microtask((){
+        context.read<ChatBloc>().stream.firstWhere((state) => !state.isAllChats);
+      });
+    }
+    return FutureBuilder(
+      future: loadAllData(context),
+      builder: (context, snapshot) {
+        final allChatState = context.watch<ChatBloc>().state;
+        if (snapshot.connectionState == ConnectionState.waiting || allChatState.isAllChats == true) {
+          return const ShimmerScreen(isHomeScreen: true,);
+        } else if (snapshot.hasError) {
+          return Center(
+            child: mediumText(
+                title: 'Ops ${allChatState.errorMessage}',
+                textAlign: TextAlign.center
+            ),
+          );
+        }else if(allChatState.allChats.isEmpty){
+          return Center(
+            child: mediumText(
+                title: 'Ops No listing found',
+                textAlign: TextAlign.center
+            ),
+          );
+        } else {
+          return SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Sized(height: 0.05),
+                const Sized(height: 0.02),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: allChatState.allChats.length,
+                  itemBuilder: (context, index) {
+                    final chat = allChatState.allChats[index];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 6.0),
+                      child: ListTile(
+                        onTap: () {
+                          // Navigate to chat screen or perform your action
+                        },
+                        leading: CircleAvatar(
+                          radius: 25,
+                          backgroundColor: AppColor.lightprimaryColor,
+                          backgroundImage: chat.chatPartner.profilePicture != null
+                              ? NetworkImage(chat.chatPartner.profilePicture!)
+                              : const AssetImage('assets/images/default_user.png') as ImageProvider,
+                        ),
+                        title: mediumText(title: chat.chatPartner.name,),
+                        subtitle: regularText(
+                          title: chat.message,
+                          maxLines: 1,
+                        ),
+                        trailing: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            lightText(title:DateHelperFunctions.timeAgo(chat.createdAt.toString()),),
+                            if (chat.isRead == 0)
+                              const Icon(
+                                Icons.mark_chat_unread,
+                                color: AppColor.secondaryColor,
+                                size: 18,
+                              )
+                          ],
+                        ),
+                        tileColor: AppColor.whiteColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          );
+        }
+      },
+    );
   }
 }
