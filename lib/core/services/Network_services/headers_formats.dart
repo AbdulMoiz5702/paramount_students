1c85b1e8495768fa6ce461e3ed6c76ca2ea6a13d
AbdulMoiz5702 <abdulmoizkoikenx10@gmail.com>
2025-06-10 09:41:53 +0500

    Hedeaer_format_added

--- lib/core/services/Network_services/headers_formats.dart
+++ lib/core/services/Network_services/headers_formats.dart
@@ -5,12 +5,7 @@
 class HeadersFormats {
 
 
-  static Map<String, String> defaultHeaders() {
-    return {
-      'Accept':'application/json',
-      'Content-Type':'application/json',
-    };
-  }
+
 
   static Map<String, String> bearerTokenHeaders({required String token}) {
     return {
