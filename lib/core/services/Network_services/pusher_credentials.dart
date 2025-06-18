1264be2978e4b2ec65ae7f11f7d41994f309af80
AbdulMoiz5702 <abdulmoizkoikenx10@gmail.com>
2025-06-18 12:57:35 +0500

    Pusher_Credentials_added

---
+++ lib/core/services/Network_services/pusher_credentials.dart
@@ -0,0 +1,11 @@
+
+
+
+
+class PusherCredentials {
+   static const String appName = 'juicy-dawn-789';
+   static const String appId = '2003268';
+   static const String key = '5ebce02aa62b4b6048ca';
+   static const String secret = 'ff76f5edd57f0ca80293';
+   static const String cluster = 'ap2';
+}
