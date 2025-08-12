6d9eb9a8ed55fd54b2214e90925f376cdf9327a7
AbdulMoiz5702 <abdulmoizkoikenx10@gmail.com>
2025-08-12 10:49:24 +0500

    test_ip

--- lib/core/services/Network_services/app_apis.dart
+++ lib/core/services/Network_services/app_apis.dart
@@ -2,7 +2,7 @@
 
 class AppApis {
   // Auth Apis
-  static const String  _ip = '192.168.1.10';
+  static const String  _ip = '192.168.1.1';
   static const String _baseUrl = 'http://$_ip:8001/api';
   static const String signup = '$_baseUrl/auth/signup';
   static const String login = '$_baseUrl/auth/login';
