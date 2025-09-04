ffe3493425eb9f054898beb9955f9361682fb2a7
AbdulMoiz5702 <abdulmoizkoikenx10@gmail.com>
2025-09-04 10:39:31 +0500

    _ip_changed

--- lib/core/services/Network_services/app_apis.dart
+++ lib/core/services/Network_services/app_apis.dart
@@ -5,7 +5,7 @@ class AppApis {
   static const String  _ip = '192.168.1.12';
   static const String _baseUrl = 'http://$_ip:8001/api';
   static const String signup = '$_baseUrl/auth/signup';
-  static const String login = '$_baseUrl/auth/loginnnnnnnnnn';
+  static const String login = '$_baseUrl/auth/login';
   static const String confirmOtp = '$_baseUrl/auth/confirm_otp?';
   static const String resendOtp = '$_baseUrl/auth/resend_otp?username=';
   static const String forgotPassword = '$_baseUrl/auth/forgot_password?username=';
